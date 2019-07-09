a6797b3c823e737856560aa5021c5ded52d6a926
JimDreamHeart <15602291936@163.com>
2019-07-09 23:04:47 +0800

    feat:初始化项目

---
+++ Assets/Scripts/CurrentTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CurrentTime : MonoBehaviour
+{
+    public Text CurrentTimeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DateTime NowTime = DateTime.Now.ToLocalTime();
+        CurrentTimeText.text = NowTime.ToString("HH:mm");
+    }
+}
